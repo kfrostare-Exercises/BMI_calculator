87f806e10db5eb40d4614b1ebaba301fe65b77b8
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-21 15:41:25 +0100

    height weight updated and cleared

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -10,8 +10,8 @@ describe('User can check their Bmi', () => {
 	it('user can enter weight and height', () => {
 		cy.visit('http://localhost:3001')
 		cy.contains('BMI Calculator')
-		cy.get('weight').type('90')
-		cy.get('height').type('186')
+		cy.get('#weight').type('90')
+		cy.get('#height').type('186')
 	})
 })
 
