a7f895556b8de94a442b603f5e98d55be9f63d64
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-21 18:33:23 +0100

    final connections ang test cleared

--- cypress/integration/usrCheckBMI.feature.js
+++ cypress/integration/usrCheckBMI.feature.js
@@ -3,13 +3,13 @@ describe('User can check their Bmi', () => {
 	{
 		cy.visit('http://localhost:3001')
 		cy.contains('BMI Calculator')
-		cy.get('#weight').type(90)
+		cy.get('#weight').type(99)
 		cy.get('#height').type(186)
 		cy.get('#Calculate').click()
 	})
 
-	it('displays a BMI value of 26.01', () => 
+	it('displays a BMI value', () => 
 	{
-		cy.get('#results').should('contain', '26.01')
+		cy.get('#results').should('contain', '28.62')
 	})
 })
